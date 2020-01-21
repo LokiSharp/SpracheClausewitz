eb96b48d69e1fb70463a499e2bbd3c7f283b3d60
LokiSharp <loki.sharp@gmail.com>
2020-01-22 00:47:29 +0800

    Update ClausewitzParser

--- Clausewitz.Dsl.Tests/ClausewitzParserTests.cs
+++ Clausewitz.Dsl.Tests/ClausewitzParserTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
 using Sprache;
 
 namespace Clausewitz.Dsl.Tests
@@ -6,28 +8,71 @@ namespace Clausewitz.Dsl.Tests
     [TestFixture]
     public class ClausewitzParserTests
     {
-        
+        [Test]
+        public void AnIdentifierIsAAssignment()
+        {
+            var input = @"happened = { prestige = 100 legitimacy = 100 stability = 3 }";
+            var parsed = ClausewitzParser.Assignment.End().Parse(input);
+            Assert.AreEqual("Hello", parsed);
+        }
+
+        [Test]
+        public void AnIdentifierIsAComment()
+        {
+            var input = "# Hello";
+            var parsed = ClausewitzParser.Comment.End().Parse(input);
+            Assert.AreEqual("Hello", parsed);
+        }
+
+        [Test]
+        public void AnIdentifierIsADate()
+        {
+            var input = "1444.11.11";
+            var parsed = ClausewitzParser.Date.End().Parse(input);
+            Assert.AreEqual(DateTime.Parse("1444-11-11"), parsed);
+        }
+
         [Test]
         public void AnIdentifierIsAInteger()
         {
             var input = "1";
-            var parsed = ClausewitzParser.INT.End().Parse(input);
+            var parsed = ClausewitzParser.Integer.End().Parse(input);
             Assert.AreEqual(1, parsed);
         }
-        
+
         [Test]
-        public void AnIdentifierIsAPCT()
+        public void AnIdentifierIsAList()
+        {
+            var input = "{ 1 1% 1.1 1444.11.11 { 2 2% 2.1 1444.11.11 abc_def } }";
+            var parsed = (List<object>) ClausewitzParser.List.End().Parse(input);
+            Assert.AreEqual(1, parsed[0]);
+        }
+
+        [Test]
+        public void AnIdentifierIsAOperator()
+        {
+            var parser = ClausewitzParser.Operator.End();
+            Assert.AreEqual("<>", parser.Parse("<>"));
+            Assert.AreEqual("<=", parser.Parse("<="));
+            Assert.AreEqual(">=", parser.Parse(">="));
+            Assert.AreEqual("<", parser.Parse("<"));
+            Assert.AreEqual(">", parser.Parse(">"));
+            Assert.AreEqual("=", parser.Parse("="));
+        }
+
+        [Test]
+        public void AnIdentifierIsAPercent()
         {
             var input = "1%";
-            var parsed = ClausewitzParser.PCT.End().Parse(input);
+            var parsed = ClausewitzParser.Percent.End().Parse(input);
             Assert.AreEqual(0.01, parsed);
         }
-        
+
         [Test]
-        public void AnIdentifierIsAREAL()
+        public void AnIdentifierIsAReal()
         {
             var input = "1.1";
-            var parsed = ClausewitzParser.REAL.End().Parse(input);
+            var parsed = ClausewitzParser.Real.End().Parse(input);
             Assert.AreEqual(1.1, parsed);
         }
     }
--- Clausewitz.Dsl/ClausewitzParser.cs
+++ Clausewitz.Dsl/ClausewitzParser.cs
@@ -1,36 +1,74 @@
 ﻿using System;
+using System.Linq;
 using Sprache;
 
 namespace Clausewitz.Dsl
 {
-    public class Item { }
-    
-    public class Map : Item { }
-    
-    public class Pair : Item { }
-    
-    public class Array : Item { }
-    
-    public class Symbol : Item { }
-
     public static class ClausewitzParser
     {
-        public static readonly Parser<int> INT =
+        private static readonly Parser<char> DoubleQuote = Parse.Char('"');
+        private static readonly Parser<string> Space = Parse.WhiteSpace.Except(Parse.LineEnd).Many().Text();
+        private static readonly Parser<char> BlockStart = Parse.Char('{');
+        private static readonly Parser<char> BlockEnd = Parse.Char('}');
+
+        private static readonly Parser<string> Symbol =
+            Parse.Identifier(Parse.Letter, Parse.LetterOrDigit.Or(Parse.Char('_')));
+
+        public static readonly Parser<string> Operator =
+            Parse.String("<>")
+                .Or(Parse.String("<="))
+                .Or(Parse.String(">="))
+                .Or(Parse.String("<"))
+                .Or(Parse.String(">"))
+                .Or(Parse.String("=")).Text();
+
+        public static readonly Parser<string> Comment =
+            from op in Parse.String("#")
+            from space in Space.Optional()
+            from comment in Parse.CharExcept('\n').Many().Text()
+            select comment;
+
+        public static readonly Parser<object> Integer =
             from op in Parse.Char('-').Optional()
             from num in Parse.Number
-            select int.Parse(num) * (op.IsDefined ? -1 : 1);
-        
-        public static readonly Parser<double> PCT =
+            select (object) (long.Parse(num) * (op.IsDefined ? -1 : 1));
+
+        public static readonly Parser<object> Percent =
             from op in Parse.Char('-').Optional()
             from num in Parse.Number
-            from pct  in Parse.Char('%')
-            select double.Parse(num) / 100 * (op.IsDefined ? -1 : 1);
-        
-        public static readonly Parser<double> REAL =
+            from pct in Parse.Char('%')
+            select (object) (double.Parse(num) / 100 * (op.IsDefined ? -1 : 1));
+
+        public static readonly Parser<object> Real =
             from op in Parse.Char('-').Optional()
             from n in Parse.Number
             from dot in Parse.Char('.')
             from f in Parse.Number
-            select double.Parse($"{n}.{f}") * (op.IsDefined ? -1 : 1);
+            select (object) (double.Parse($"{n}.{f}") * (op.IsDefined ? -1 : 1));
+
+        public static readonly Parser<object> Date =
+            from y in Parse.Number
+            from yDot in Parse.Char('.')
+            from m in Parse.Number
+            from dDot in Parse.Char('.')
+            from d in Parse.Number
+            select (object) DateTime.Parse($"{y}-{m}-{d}");
+
+        public static readonly Parser<object> Assignment =
+            from name in Symbol
+            from s1 in Space.Optional()
+            from op in Operator
+            from s2 in Space.Optional()
+            from value in List.Or(Date).Or(Percent).Or(Real).Or(Integer).Or(Symbol)
+            select new Tuple<object, object, object>(name, op, value);
+
+        public static readonly Parser<object> List =
+            from bs in BlockStart
+            from leading in Space.Optional()
+            from values in Parse.Ref(() => List.Or(Assignment).Or(Date).Or(Percent).Or(Real).Or(Integer).Or(Symbol))
+                .DelimitedBy(Space)
+            from trailing in Space.Optional()
+            from be in BlockEnd
+            select values.ToList();
     }
 }
