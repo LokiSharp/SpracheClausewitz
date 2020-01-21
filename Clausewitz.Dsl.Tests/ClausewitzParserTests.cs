64c4e2599a92777f7a80d2cc3133bd33858fefd7
LokiSharp <loki.sharp@gmail.com>
2020-01-21 17:12:05 +0800

    Update workflow

---
+++ Clausewitz.Dsl.Tests/ClausewitzParserTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Sprache;
+
+namespace Clausewitz.Dsl.Tests
+{
+    [TestFixture]
+    public class ClausewitzParserTests
+    {
+        
+        [Test]
+        public void AnIdentifierIsAInteger()
+        {
+            var input = "1";
+            var parsed = ClausewitzParser.INT.End().Parse(input);
+            Assert.AreEqual(1, parsed);
+        }
+        
+        [Test]
+        public void AnIdentifierIsAPCT()
+        {
+            var input = "1%";
+            var parsed = ClausewitzParser.PCT.End().Parse(input);
+            Assert.AreEqual(0.01, parsed);
+        }
+        
+        [Test]
+        public void AnIdentifierIsAREAL()
+        {
+            var input = "1.1";
+            var parsed = ClausewitzParser.REAL.End().Parse(input);
+            Assert.AreEqual(1.1, parsed);
+        }
+    }
+}
---
+++ Clausewitz.Dsl/ClausewitzParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Sprache;
+
+namespace Clausewitz.Dsl
+{
+    public class Item { }
+    
+    public class Map : Item { }
+    
+    public class Pair : Item { }
+    
+    public class Array : Item { }
+    
+    public class Symbol : Item { }
+
+    public static class ClausewitzParser
+    {
+        public static readonly Parser<int> INT =
+            from op in Parse.Char('-').Optional()
+            from num in Parse.Number
+            select int.Parse(num) * (op.IsDefined ? -1 : 1);
+        
+        public static readonly Parser<double> PCT =
+            from op in Parse.Char('-').Optional()
+            from num in Parse.Number
+            from pct  in Parse.Char('%')
+            select double.Parse(num) / 100 * (op.IsDefined ? -1 : 1);
+        
+        public static readonly Parser<double> REAL =
+            from op in Parse.Char('-').Optional()
+            from n in Parse.Number
+            from dot in Parse.Char('.')
+            from f in Parse.Number
+            select double.Parse($"{n}.{f}") * (op.IsDefined ? -1 : 1);
+    }
+}
